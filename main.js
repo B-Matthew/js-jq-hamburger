905cb348063b8701d5f562db7ea6b3a8679b0ab2
Matthew <matsgeco92@hotmail.it>
2021-03-18 13:59:38 +0100

    Esercizio finito

---
+++ main.js
@@ -0,0 +1,13 @@
+// Creare un hamburger menu con l'utilizzo di Html, Css e JavaScript. Scrivere solo la parte con jQuery senza modiﬁcare HTML e CSS.
+// N.B.: nessuna modifica consentita a HTML o CSS
+
+function dropped() {
+  $(".hamburger-menu").show();
+}
+
+function dropped2(){
+  $(".hamburger-menu").hide();
+}
+
+$('.fa-bars').click(dropped);
+$('.fa-times').click(dropped2);
